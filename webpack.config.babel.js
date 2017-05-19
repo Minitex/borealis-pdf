de6dcbee9ce8f010fd1bc71f5ae3c0bb6f0e7367
chadfennell <libsys@gmail.com>
2017-05-19 15:21:58 -0700

    point webpack build at borealis-pdf; a little es6 linting

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,69 +1,69 @@
-var webpack = require("webpack");
-var path = require('path');
-var merge = require('webpack-merge')
+const webpack = require('webpack');
+const path = require('path');
+const merge = require('webpack-merge');
 
-var TARGET = process.env.npm_lifecycle_event;
-var minimize = (process.argv.indexOf('--minimize') > 0) ? true : false;
-var build =    (process.argv.indexOf('--build') > 0) ? true : false;
+const TARGET = process.env.npm_lifecycle_event;
+const minimize = process.argv.indexOf('--minimize') > 0;
+const build = process.argv.indexOf('--build') > 0;
 
-var common = {
+const common = {
   module: {
     loaders: [
       {
         test: /\.js?$/,
         loader: 'react-hot-loader',
-        include: path.join(__dirname, 'src')
+        include: path.join(__dirname, 'src'),
       },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
-          presets: ["react", "es2015", "stage-0"]
-        }
+          presets: ['react', 'es2015', 'stage-0'],
+        },
       },
       {
-          test: /\.css$/,
-          loaders: ["style-loader", "css-loader"]
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
       },
       {
-          test: /\.less$/,
-          loaders: ["style-loader", "css-loader"]
-      }
-    ]
+        test: /\.less$/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
-      new webpack.HotModuleReplacementPlugin()
-  ]
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
 
 if (build) {
   if (minimize) {
     common.plugins.push(new webpack.optimize.UglifyJsPlugin());
     var config =
-    {
-      entry: './src/react-openseadragon.js',
-      output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'react-openseadragon-min.js',
-        library: "react-openseadragon",
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-      }
-    }
+      {
+        entry: './src/borealis-pdf.js',
+        output: {
+          path: path.join(__dirname, 'dist'),
+          filename: 'borealis-pdf.js',
+          library: 'borealis-pdf',
+          libraryTarget: 'umd',
+          umdNamedDefine: true
+        },
+      };
   } else {
     var config =
-    {
-      devtool: 'source-map',
-      entry: './src/react-openseadragon.js',
-      output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'react-openseadragon.js',
-        library: "react-openseadragon",
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-      }
-    }
+      {
+        devtool: 'source-map',
+        entry: './src/borealis-pdf.js',
+        output: {
+          path: path.join(__dirname, 'dist'),
+          filename: 'borealis-pdf.js',
+          library: 'borealis-pdf',
+          libraryTarget: 'umd',
+          umdNamedDefine: true,
+        },
+      };
   }
   module.exports = merge(common, config);
 }
@@ -73,8 +73,8 @@ if (TARGET === 'start') {
     devtool: 'source-map',
     entry: './example/example.js',
     output: {
-        path: path.join(__dirname, 'docs'),
-        filename: "example.js"
-    }
+      path: path.join(__dirname, 'docs'),
+      filename: 'example.js',
+    },
   });
 }
